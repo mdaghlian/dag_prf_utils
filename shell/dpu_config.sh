9511a515f06a838f15de5dafcd3449138affaa9d
mdaghlian <marcusdag@googlemail.com>
2025-01-08 14:30:19 +0100

    easier fast install for everything

---
+++ shell/dpu_config.sh
@@ -0,0 +1,10 @@
+# 
+# specify the folder where dag_prf_utils is held
+DPU_SETUP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DPU_SETUP_FILE="${DPU_SETUP_DIR}/dpu_setup"
+DPU_REPO_DIR=$(dirname ${DPU_SETUP_DIR})
+export DAG_UTILS=$DPU_REPO_DIR
+
+# Configurartion for DPU setup
+install_git_files="True" # True or False
+install_cmd="pip install -r" # or "conda install -y --file " "mamba install -y --file "
